1d88a4f4e0f244f81afadaf3270ff4f5d159d4a1
McGiry <maxgiriy7@gmail.com>
2020-11-04 20:16:08 +0200

    Lesson 4, tasks 1 - 3 commit

---
+++ MaxGiriy_Lesson04/MaxGiriy_Lesson04_Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MaxGiriy_Lesson04_Task01 {
+
+    //Гирий Максим
+    //    1. Дан целочисленный массив из 20 элементов.Элементы массива могут принимать целые
+    //    значения от –10 000 до 10 000 включительно.Заполнить случайными числами.Написать
+    //    программу, позволяющую найти и вывести количество пар элементов массива, в которых только
+    //    одно число делится на 3. В данной задаче под парой подразумевается два подряд идущих
+    //    элемента массива.Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2.
+
+    class Program {
+
+        static void Main(string[] args) {
+            int[] arr = new int[20];
+            FillArray(arr);
+            int counter = 0;
+            for (int i = 0; i < arr.Length - 1; ++i) {
+                if (IsRequiredPair(arr[i], arr[i + 1])) counter++;
+            }
+            Console.WriteLine($"The number of required pairs is {counter}");
+            Console.ReadKey();
+        }
+
+        static void FillArray(int[] arr) {
+            int maxVal = 10_000, minVal = -10_000;
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; ++i) {
+                arr[i] = rand.Next(minVal, maxVal);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static bool IsRequiredPair(int a, int b) {
+            return a % 3 == 0 && b % 3 != 0 || a % 3 != 0 && b % 3 == 0;
+        }
+    }
+}
---
+++ MaxGiriy_Lesson04/MaxGiriy_Lesson04_Task03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyArrayLib;
+
+namespace MaxGiriy_Lesson04_Task03 {
+
+    //Гирий Максим
+    //    3. а) Дописать класс для работы с одномерным массивом.Реализовать конструктор, создающий
+    //    массив определенного размера и заполняющий массив числами от начального значения с
+    //    заданным шагом.Создать свойство Sum, которое возвращает сумму элементов массива, метод
+    //    Inverse, возвращающий новый массив с измененными знаками у всех элементов массива(старый
+    //    массив, остается без изменений), метод Multi, умножающий каждый элемент массива на
+    //    определённое число, свойство MaxCount, возвращающее количество максимальных элементов.
+    //    б)** Создать библиотеку содержащую класс для работы с массивом.Продемонстрировать работу
+    //    библиотеки
+    //    е) *** Подсчитать частоту вхождения каждого элемента в массив(коллекция Dictionary<int, int>)
+
+    class Program {
+
+        static void Main(string[] args) {
+            MyArray arr = new MyArray(5, 2f, 3);
+            Console.WriteLine(arr.ToString());
+            Console.WriteLine(arr.Sum);
+            MyArray copy = arr.Inverse();
+            Console.WriteLine(copy.ToString());
+            arr.Multi(2);
+            Console.WriteLine(arr.ToString());
+            Console.WriteLine(arr.MaxCount);
+            Console.WriteLine();
+
+            Dictionary<int, int> elemsCounter = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Size; ++i) {
+                if (elemsCounter.ContainsKey(arr[i])) { elemsCounter[arr[i]]++;
+                } else elemsCounter.Add(arr[i], 1);
+            }
+            foreach (var item in elemsCounter) {
+                Console.WriteLine($"{item.Key, 2} - {item.Value}");
+            }
+        }
+
+    }
+}
